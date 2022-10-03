7fda2e800833a3cefe6fdbd420c19afb4cd5ea3f
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2022-10-02 23:25:15 -0300

    Note class added. Unitary tests added

--- mapping_single.py
+++ mapping_single.py
@@ -35,5 +35,20 @@ class User(Base):
         return f"<User username={self.username} email={self.email}>"
 
 
+class Note(Base):
+    __tablename__ = 'notes'
+    id_note = Column(Integer(), primary_key=True)
+    notename = Column(String(40), nullable=False, unique=True)
+    usernotes = Column(String(80), unique=True, nullable=False)
+    date_created = Column(DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Note notename={self.notename} usernotes={self.usernotes}>"
+
+
+# TESTES DE USER STORIES
 new_user = User(id_user=1, username="luis", email="luis@gmail.com")
 print(new_user)
+
+new_note = Note(id_note=1, notename="Minha Primeira Anotação", usernotes="nova anotação aqui")
+print(new_note)
